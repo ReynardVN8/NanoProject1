d2305c5dc9694eb71220244a018acc7cab92b98e
Reynard Vincent Nata <reynardvnata@yahoo.co.id>
2019-05-20 07:32:14 +0700

    Initial Commit

---
+++ NanoProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoProject1
+//
+//  Created by Reynard Vincent Nata on 20/05/19.
+//  Copyright © 2019 Reynard Vincent Nata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
